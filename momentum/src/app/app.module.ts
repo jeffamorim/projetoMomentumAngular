c597514464264287b2c0d1f59c404fcf6b3b3133
Jeferson Amorim <jeferson.amorim@live.com>
2020-05-12 20:05:10 -0300

    Ajuste no component footer

--- momentum/src/app/app.module.ts
+++ momentum/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { SbrenosComponent } from './sbrenos/sbrenos.component';
     AppRoutingModule,
     FontAwesomeModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- momentum/src/app/footer/footer.component.ts
+++ momentum/src/app/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
