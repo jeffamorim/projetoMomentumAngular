9c6296721e3eb6d3e2559d8e30f71baa91c596dc
Denise Pereira <denisefeds@hotmail.com>
2020-05-12 18:03:03 -0300

    Implementação do Footer

--- momentum/src/app/app.module.ts
+++ momentum/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
