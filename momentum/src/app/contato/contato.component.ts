031e8011e56268e51a3e38999b500f98a8cb249b
Alexandre <alexandremarcopoulos28@gmail.com>
2020-05-19 20:26:28 -0300

    Implementaçao das regras de cadastro

--- momentum/src/app/contato/contato.component.ts
+++ momentum/src/app/contato/contato.component.ts
@@ -22,3 +22,4 @@ export class ContatoComponent implements OnInit {
   }
 
 }
+
--- momentum/src/app/formulario/formulario.component.ts
+++ momentum/src/app/formulario/formulario.component.ts
@@ -13,3 +13,31 @@ export class FormularioComponent implements OnInit {
   }
 
 }
+
+function validacao (){
+  let nome = (<HTMLSelectElement>document.getElementById('nome')).value;
+  let email = (<HTMLSelectElement>document.getElementById('email')).value;
+  let senha = (<HTMLSelectElement>document.getElementById('senha')).value;
+  let confirmaSenha = (<HTMLSelectElement>document.getElementById('confirmacaoSenha')).value; 
+  let tamSenha = senha.length;
+  let tamConfSenha = confirmaSenha.length;
+  
+  if (tamSenha > 6 || tamConfSenha > 6 && tamSenha != null || tamConfSenha != null) {
+  if (senha == confirmaSenha) {
+  document.writeln("Cadastro realizado");
+  }
+  else{
+  document.writeln("As senhas não são iguais!");
+  } 
+  }
+  else{
+  alert("A senha deve ter no minimo 6 caracteres");
+  }
+  }
+  
+  document.getElementById('btnenviar').addEventListener('click',() =>{
+  
+  validacao();
+  
+  });
+  
