099df2d7352753d65efdbbaba43b37ce5be2b970
Magno <magnoneves2@gmail.com>
2020-06-12 16:04:26 -0300

    Conclusão da criação dos métodos GET, POST, PUT, DELETE

--- backend/redesocial/redesocial/src/main/java/momentum/redesocial/controller/PostagemController.java
+++ backend/redesocial/redesocial/src/main/java/momentum/redesocial/controller/PostagemController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,7 @@ public class PostagemController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/texto/{texto} ")
+	@GetMapping("/texto/{texto}")
 	public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto) {
 		return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
 	}
@@ -44,4 +46,15 @@ public class PostagemController {
 	public ResponseEntity<Postagem> postPublicacao(@RequestBody Postagem texto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(texto));
 	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putPublicacao(@RequestBody Postagem texto) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(texto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePublicacao(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
 }
