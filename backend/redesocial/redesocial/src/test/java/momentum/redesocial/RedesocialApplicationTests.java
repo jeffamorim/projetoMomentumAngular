55b9e06db8ce9319161d4cbd745b1b00e612c231
Magno <magnoneves2@gmail.com>
2020-06-12 02:09:31 -0300

    Implementação da estrutura básica da API em Spring Boot

---
+++ backend/redesocial/redesocial/src/test/java/momentum/redesocial/RedesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package momentum.redesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
